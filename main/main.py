060cfe70fce2f104de669c86799aed6cf0e137fb
victor-PC\victor <victor.m.pelaez@outlook.com>
2016-12-09 22:20:02 +0100

    Initial commit

---
+++ main/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created by: Victor Peláez  09-12-2016
+"""
+
+import numpy as np 
+import pandas as pd
+
+#pd.read_csv(r"..\input\test.csv")
+
+df = pd.read_hdf(r"..\input\train.h5")
